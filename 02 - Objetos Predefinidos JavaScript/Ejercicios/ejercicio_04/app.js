45570d5d49b274458a21de58616b51973241179d
TaniaAlcudia <tanialcudia@gmail.com>
2021-09-24 17:57:32 +0200

    Ejercicios Tema2 Terminados

--- 02 - Objetos Predefinidos JavaScript/Ejercicios/ejercicio_04/app.js
+++ 02 - Objetos Predefinidos JavaScript/Ejercicios/ejercicio_04/app.js
@@ -10,17 +10,58 @@ Google. Dicha ventana debe tener las características siguientes:
 - El ancho de la ventana es de 600 y el alto de 400 
 - Las coordenadas son top=50 y left=150*/
 
-window.onload=function()
+function detectarBrowser()
 {
-    mostrarInfoScreen();
-    establecerEstilo();
+    let browser = "Desconocido";
+
+    if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 )
+        browser = "Opera";
+    else if(navigator.userAgent.indexOf("Chrome") != -1 ) 
+        browser = "Chrome";
+    else if(navigator.userAgent.indexOf("Safari") != -1) 
+        browser = "Safari";
+    else if(navigator.userAgent.indexOf("Firefox") != -1 )
+        browser = "Firefox";
+    else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) 
+        browser = "IE";
+    
+    return browser;   
 }
 
-function mostrarInfoScreen()
+function detectarSistema()
 {
+    let sistema = "Desconocido";
+    
+    if (navigator.appVersion.indexOf("Win") != -1) 
+        sistema = "Windows OS";
+    else if (navigator.appVersion.indexOf("Mac") != -1) 
+        sistema = "MacOS";
+    else if (navigator.appVersion.indexOf("X11") != -1) 
+        sistema = "UNIX OS";
+    else if (navigator.appVersion.indexOf("Linux") != -1) 
+        sistema = "Linux OS";
+
+    return sistema;
+}
+
+function mostrarInfo()
+{
+    // screen
     let anchoPantalla = window.screen.width;
     let altoPantalla = window.screen.height;
     document.getElementById("screen").innerHTML=`La resolución de la pantalla es ${anchoPantalla}x${altoPantalla}`;
+
+    //navigator
+    document.getElementById("navigator").innerHTML = detectarBrowser();
+    document.getElementById("sistema").innerHTML = detectarSistema();
+    document.getElementById("version").innerHTML = navigator.appVersion;
+
+    //document
+    document.getElementById("document").innerHTML = document.location.pathname;
+
+    //location
+    document.getElementById("location").innerHTML = document.location.protocol;
+
 }
 
 function establecerEstilo()
@@ -38,4 +79,10 @@ function establecerEstilo()
         document.getElementById("rojo").style.border='2px solid #EB413D';
         document.getElementById("verde").style.border='2px solid #52A16D';
         document.getElementById("amarillo").style.border='2px solid #F2BE3D';
+}
+
+window.onload = () =>
+{
+    mostrarInfo();
+    establecerEstilo();
 }
--- 02 - Objetos Predefinidos JavaScript/Ejercicios/ejercicio_10/app.js
+++ 02 - Objetos Predefinidos JavaScript/Ejercicios/ejercicio_10/app.js
@@ -1,6 +1,28 @@
 //Crea una página con un botón que al pulsarlo se abran 5 ventanas emergentes de 
 //dimensiones 200x200 cada una de ellas con un botón para cerrarla.
 
+const nVentanas = 5;
+const tamanio = 200;
+const ventanas = [];
 
+const boton = document.getElementById("btn");
+boton.onclick = () => 
+{
+    for (let cont = 0; cont < nVentanas; cont++)
+    {
+        // abrir ventana
+        ventanas[cont] = window.open("", "_blank", `height=${tamanio}, width=${tamanio}`);
 
- 
+        //crear boton
+        const btnCerrar = ventanas[cont].document.createElement("button");
+        btnCerrar.innerHTML = "Cerrar";
+
+        // añadirle funcion cerrar al hacer click
+        btnCerrar.onclick = () => ventanas[cont].close();
+        
+        // añadir el boton al body
+        const ventanaBody = ventanas[cont].document.querySelector("body");
+        ventanaBody.appendChild(btnCerrar);
+    }
+}
+
