0656f3f3150022266da9c63f43c0fcb5eca2f2a2
TaniaAlcudia <tanialcudia@gmail.com>
2021-10-13 20:03:57 +0200

    04 TANDA TERMINADA

--- 04 - Funciones Arrays Objetos/Ejercicios/ejercicio_10/app.js
+++ 04 - Funciones Arrays Objetos/Ejercicios/ejercicio_10/app.js
@@ -8,54 +8,46 @@ function tipoLetras(str)
 {
     let esMinuscula = false;
     let esMayuscula = false;
-    let esMixto = false;
-    let mensaje = "";
 
-    for (let i = 0; i < str.length; i++) 
+    if (str.length < 0)
+        alert("Campo Vacio");
+    else
     {
-        if (str.charCodeAt(i) <= 'z'.charCodeAt(0) && str.charCodeAt(i) >= 'a'.charCodeAt(0))
+        for (let i = 0; i < str.length; i++) 
         {
-            if (esMayuscula)
-            {
-                esMixto = true;
-                break;
-            }
-
-            esMinuscula = true;
+            if (str.charCodeAt(i) <= 'z'.charCodeAt(0) && str.charCodeAt(i) >= 'a'.charCodeAt(0))
+             esMinuscula = true;
+            else if (str.charCodeAt(i) <= 'Z'.charCodeAt(0) && str.charCodeAt(i) >= 'A'.charCodeAt(0)) 
+                esMayuscula = false;
         }
-        else if (str.charCodeAt(i) <= 'Z'.charCodeAt(0) && str.charCodeAt(i) >= 'A'.charCodeAt(0))
-        {
-            if (esMinuscula)
-            {
-                esMixto = true;
-                break;
-            }
 
-            esMayuscula = false;
-        }    
+        let mensaje = "no son letras";
+
+        if (esMayuscula && esMinuscula)
+            mensaje = "mayusculas y minusculas";
+        else if (esMinuscula)
+            mensaje = "minusculas";
+        else if (esMayuscula)
+            mensaje = "mayusculas";
+
+        const ventana = window.open("", "_blank", "width=300,height=300");
+
+        const pHtml = ventana.document.createElement("p");
+        pHtml.innerHTML = "Tipo de letra: " + mensaje;
+        
+        const btnHtml = ventana.document.createElement("button");
+        btnHtml.innerHTML = "CERRAR";
+        
+        btnHtml.onclick = () => window.close();
+
+        ventana.document.body.appendChild(pHtml);
+        ventana.document.body.appendChild(btnHtml);
     }
-
-    if (esMixto)
-        mensaje = "Mixto";
-    else if (esMayuscula)
-        mensaje = "Mayusculas";
-    else if (esMinuscula)
-        mensaje = "Minusculas";
-    elses
-        mensaje = "Glifos";
-
-
-    const ventana = window.open("", "Tipo de Letra", "width=300,height=300");
-    const pHtml = ventana.document.querySelector("p");
-    pHtml.innerHTML = "Tipo de Letra: " + mensaje;
-      
-    const btnHtml = ventana.document.querySelector("button");
-    btnHtml.innerHTML = "CERRAR";
     
-    btnHtml.onclick = () => window.close();
 }
 
-window.onload = () => {
+window.onload = () => 
+{
     const btnValidarHtml = document.getElementById("btn-validar");
     const textoHtml = document.getElementById("texto");
     btnValidarHtml.onclick = () => tipoLetras(textoHtml.value);
