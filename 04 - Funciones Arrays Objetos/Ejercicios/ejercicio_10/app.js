06685c53117cec6ef23074c4ae8729a4f5f4e9b2
Tania Alcudia <tanialcudia@gmail.com>
2021-10-13 12:35:24 +0200

    0410 TERMINADO

--- 04 - Funciones Arrays Objetos/Ejercicios/ejercicio_10/app.js
+++ 04 - Funciones Arrays Objetos/Ejercicios/ejercicio_10/app.js
@@ -4,3 +4,55 @@
 // mezcla de ambas. El resultado se mostrará en una nueva ventana con un botón cerrar que 
 // cerrara dicha ventana
 
+function tipoLetras(str)
+{
+    let esMinuscula = false;
+    let esMayuscula = false;
+    let esMixto = false;
+
+    for (let i = 0; i < str.length; i++) 
+    {
+        if (str.charCodeAt(i) <= 'z'.charCodeAt(0) && str.charCodeAt(i) >= 'a'.charCodeAt(0))
+        {
+            if (esMayuscula)
+            {
+                esMixto = true;
+                break;
+            }
+
+            esMinuscula = true;
+        }
+        else if (str.charCodeAt(i) <= 'Z'.charCodeAt(0) && str.charCodeAt(i) >= 'A'.charCodeAt(0))
+        {
+            if (esMinuscula)
+            {
+                esMixto = true;
+                break;
+            }
+
+            esMayuscula = false;
+        }    
+    }
+
+    if (esMixto)
+        abrirVentana("Mixto");
+    else if (esMayuscula)
+        abrirVentana("Mayusculas");
+    else if (esMinuscula)
+        abrirVentana("Minusculas");
+    else
+        abrirVentana("Glifos");
+}
+
+function abrirVentana(tipoDeLetra)
+{
+    const ventana = window.open("./mayus.html", "Tipo de Letra", "width=300,height=300");
+    console.log("LA PUTA MADREEEEEEEEEEEEEEEEEEEEEEEEEEEEEE");
+  
+}   
+
+window.onload = () => {
+    const btnValidarHtml = document.getElementById("btn-validar");
+    const textoHtml = document.getElementById("texto");
+    btnValidarHtml.onclick = () => tipoLetras(textoHtml.value);
+}
---
+++ 04 - Funciones Arrays Objetos/Ejercicios/ejercicio_11/app.js
@@ -0,0 +1,4 @@
+// Juego del número secreto. La página generará un número al azar entre 1 y 100 (ambos 
+// inclusive).El usuario irá introduciendo números por teclado y el ordenador le irá dando pistas: 
+// “mi número es menor” o “mi número es mayor”, hasta que el usuario acierte. Entonces se le 
+// felicitará y se indicará el número de intentos que necesitó para acertar el número secreto.
